011b9404b43215a27f849d97bbc8065dd8e9e23c
AskatAsh <abdulaskat@gmail.com>
2025-06-29 13:56:06 +0600

    connected server to mongodb using express

---
+++ backend/server.js
@@ -0,0 +1,38 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const express = require('express');
+const uri = "mongodb+srv://mongodb:learning_backend_mongodb@cluster01.d7f8blu.mongodb.net/eventManagementDB?retryWrites=true&w=majority&appName=Cluster01";
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Event Management Server is Running...");
+});
+
+app.listen(port, () => {
+  console.log("Server is Running on port: ", port);
+});

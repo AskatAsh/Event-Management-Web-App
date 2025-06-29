718538fbb7c2fc9d0637c6a81c316791d25a58ad
AskatAsh <abdulaskat@gmail.com>
2025-06-29 14:05:32 +0600

    added events route to get recent events

--- backend/server.js
+++ backend/server.js
@@ -18,13 +18,23 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const eventsCollection = client.db("eventManagementDB").collection("events");
+
+    app.get('/events', async (req, res) => {
+      const result = await eventsCollection.find({}).sort({"dateTime": -1}).limit(3).toArray();
+      console.log(result);
+      res.send(result);
+    })
+
+
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);

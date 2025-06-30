ea1cf390e57e7048729cde98fa40aa5787dfa739
AskatAsh <abdulaskat@gmail.com>
2025-06-30 23:27:30 +0600

    added vercel.json file and remove console.logs and port listeners

--- backend/server.js
+++ backend/server.js
@@ -455,7 +455,7 @@ async function run() {
 
     // Send a ping to confirm a successful connection
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -467,6 +467,6 @@ app.get("/", (req, res) => {
   res.send("Event Management Server is Running...");
 });
 
-app.listen(port, () => {
-  console.log("Server is Running on port: ", port);
-});
+// app.listen(port, () => {
+//   console.log("Server is Running on port: ", port);
+// });
